ea88fc4897f02529152f7148e1bef39632b753c7
monica dolce <106848108+monicadolce@users.noreply.github.com>
2022-09-18 13:04:30 -0400

    added get routes to category-routes.js

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,14 +3,30 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categories = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categories);
+  } catch (err) {
+    res.status(400).json(err);    
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const category = await Category.findByPk(req.params.id, {
+      include: [{ model: Product}],
+    });
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(400).json(err); 
+  }
 });
 
 router.post('/', (req, res) => {

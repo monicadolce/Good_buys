0099f195314f4b61b5aa37ab045e2a1967d2f304
monica dolce <106848108+monicadolce@users.noreply.github.com>
2022-09-18 10:27:15 -0400

    working on index.js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,22 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category.id',
+});
 // Categories have many Products
-
+Category.hasMany(Product), {
+  foreignKey: 'category.id',
+  onDelete: 'CASCADE',
+};
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  foreignKey: 'product_tag',
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  foreignKey: 'product_tag',
+}),
 
 module.exports = {
   Product,

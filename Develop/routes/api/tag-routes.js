acb723251525f3ddde169d78c002e7c98fe315ef
monica dolce <106848108+monicadolce@users.noreply.github.com>
2022-09-18 14:52:22 -0400

    created post, put, delete routes in tag-routes.js

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -29,34 +29,110 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
-  Tag.create(req.body)
-  .then((tag) => {
-    if (req.body.tagIds.length) {
-      const TagIdArr = req.body.tagIds.map((product_id) => {
-        return {
-          tag_id: tag.id,
-          product_id,
-        };
-      });
-      return ProductTag.bulkCreate(TagIdArr);
-    }
-    res.status(200).json(tag);
-  })
-  .then((TagIds) => res.status(200).json(TagIds))
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
-  });
+
+// First attempt:
+try {
+  const newTag = await Tag.create(req.body);
+  res.status(200).json(newTag);
+} catch (err) {
+  res.status(400).json(err);
+}
 });
 
-router.put('/:id', (req, res) => {
+// Second attempt:
+//   Tag.create(req.body)
+//   .then((tag) => {
+//     if (req.body.tagIds.length) {
+//       const TagIdArr = req.body.tagIds.map((product_id) => {
+//         return {
+//           tag_id: tag.id,
+//           product_id,
+//         };
+//       });
+//       return ProductTag.bulkCreate(TagIdArr);
+//     }
+//     res.status(200).json(tag);
+//   })
+//   .then((TagIds) => res.status(200).json(TagIds))
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(400).json(err);
+//   });
+// });
+
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const updatedTag = await Tag.update({
+      where: {
+        id:req.params.id,
+      },
+    });
+    res.status(200).json(updatedTag);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
+// Second attempt:
+//   Tag.update(req.body, {
+//     where: {
+//       id: req.params.id,
+//     },
+//   })
+//     .then((tag) => {
+//       // find all associated tags from ProductTag
+//       return ProductTag.findAll({ where: { product_id: req.params.id } });
+//     })
+//     .then((productTags) => {
+//       // get list of current tag_ids
+//       const productTagIds = productTags.map(({ tag_id }) => tag_id);
+//       // create filtered list of new tag_ids
+//       const newProductTags = req.body.tagIds
+//         .filter((tag_id) => !productTagIds.includes(tag_id))
+//         .map((tag_id) => {
+//           return {
+//             tag_id: req.params.id,
+//             product_id,
+//           };
+//         });
+//       // figure out which ones to remove
+//       const productTagsToRemove = productTags
+//         .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
+//         .map(({ id }) => id);
 
-router.delete('/:id', (req, res) => {
+//       // run both actions
+//       return Promise.all([
+//         ProductTag.destroy({ where: { id: productTagsToRemove } }),
+//         ProductTag.bulkCreate(newProductTags),
+//       ]);
+//     })
+//     .then((updatedProductTags) => res.json(updatedProductTags))
+//     .catch((err) => {
+//       // console.log(err);
+//       res.status(400).json(err);
+//     });
+
+
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const deletedTag = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!deletedTag) {
+      res.status(404).json({ message: 'No tag found with that id!' });
+      return;
+    }
+    
+    res.status(200).json(deletedTag);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;

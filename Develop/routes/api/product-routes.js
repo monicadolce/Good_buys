d7d056616d9e9e7909f7fe76f27b283994256f8f
monica dolce <106848108+monicadolce@users.noreply.github.com>
2022-09-18 12:57:59 -0400

    updated delete route in product-routes.js

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -7,6 +7,7 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  // Updated:
   try {
     const products = await Product.findAll({
       include: [{ model: Category }, { model: Tag }],
@@ -17,6 +18,7 @@ router.get('/', async (req, res) => {
   }
 });
 
+// Updated:
 // get one product
 router.get('/:id', async (req, res) => {
   // find a single product by its `id`
@@ -105,8 +107,25 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+// Updated:
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const deletedProduct = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!deletedProduct) {
+      res.status(404).json({ message: 'No product found with that id!' });
+      return;
+    }
+    
+    res.status(200).json(deletedProduct);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;

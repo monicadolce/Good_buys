d1167ce155e0f28c3ce117fc6c3d5dfed6d60b34
monica dolce <106848108+monicadolce@users.noreply.github.com>
2022-09-18 13:43:38 -0400

    created get routes in tag-routes.js

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -3,14 +3,30 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tags = await Tag.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(tags);
+  } catch (err) {
+    res.status(400).json(err);    
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tag = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    res.status(200).json(tag);
+  } catch (err) {
+    res.status(400).json(err); 
+  }
 });
 
 router.post('/', (req, res) => {

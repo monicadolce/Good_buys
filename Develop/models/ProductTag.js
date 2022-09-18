6557e3ba434d37bf4da852ce45d5ed5b51e2ce59
monica dolce <106848108+monicadolce@users.noreply.github.com>
2022-09-18 10:11:10 -0400

    added references to productTag.js

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -14,11 +14,17 @@ ProductTag.init(
     },
     product_id: {
       type: DataTypes.INTEGER,
-      //     * References the `Product` model's `id`.
+      references: {
+        model: 'product',
+        key: 'id',
+      },
     },
     tag_id: {
       type: DataTypes.INTEGER,
-      //     * References the `Tag` model's `id`.
+      references: {
+        model: 'tag',
+        key: 'id',
+      },
     },
   },
   {

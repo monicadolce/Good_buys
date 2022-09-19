55868df4c185b6f12499325d4f5d0c82d69e3a3d
monica dolce <106848108+monicadolce@users.noreply.github.com>
2022-09-19 14:06:58 -0400

    removed commented out code from product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,7 +7,6 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
-  // Updated:
   try {
     const products = await Product.findAll({
       include: [{ model: Category }, { model: Tag }],
@@ -18,7 +17,6 @@ router.get('/', async (req, res) => {
   }
 });
 
-// Updated:
 // get one product
 router.get('/:id', async (req, res) => {
   // find a single product by its `id`
@@ -107,7 +105,6 @@ router.put('/:id', (req, res) => {
     });
 });
 
-// Updated:
 router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
   try {
